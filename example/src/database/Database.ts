9293405f714ae210e2d3bf8660446665daf32a4e
Lukas R <45606977+lukasapp@users.noreply.github.com>
2020-04-21 21:31:28 +0200

    Update Database.ts

--- example/src/database/Database.ts
+++ example/src/database/Database.ts
@@ -1,76 +1,12 @@
-import SQLite, {
-  SQLiteDatabase,
-  SQLError,
-  ResultSet,
-  Transaction,
-} from 'react-native-sqlite-storage';
+import AsyncStorageFactory from '@react-native-community/async-storage';
+import LegacyStorage from '@react-native-community/async-storage-backend-legacy';
 
-import {name as nameDB} from '../../package.json';
-/**
- * @class Database
- * @singleton
- */
-export default class Database {
-  private db: SQLiteDatabase;
-  private static instance: Database;
-
-  /**
-   * Returns the instance of this class.
-   *
-   * @returns {Database}
-   */
-  public static getInstance(): Database {
-    if (this.instance == null) {
-      this.instance = new Database();
-    }
-
-    return this.instance;
-  }
-
-  public setConfiguration(key: string, value: any): void {
-    // TODO: insert into
-  }
-
-  /**
-   * Creates the tables in the SQLite database.
-   */
-  private createTables(): void {
-    this.db.executeSql(
-      `
-      CREATE TABLE IF NOT EXISTS Configuration (
-        ID INT PRIMARY KEY NOT NULL,
-        KEY CHAR(50) NOT NULL UNIQUE,
-        VALUE TEXT NOT NULL
-     );`,
-      [],
-      (transaction: Transaction, resultSet: ResultSet) => {},
-      (transaction: Transaction, error: SQLError) =>
-        console.error(
-          `Failed to create database tables: ${error.code} -> ${error.message}`,
-        ),
-    );
-  }
-
-  /**
-   * Connects to the SQLite database.
-   */
-  private constructor() {
-    this.db = SQLite.openDatabase(
-      {
-        name: nameDB,
-        location: 'Library',
-      },
-      () => console.log(`Connected to ${nameDB} SQLite`),
-      (err: SQLError) => {
-        throw new Error(
-          JSON.stringify({
-            msg: `Failed to connect ${nameDB} SQLite`,
-            err,
-          }),
-        );
-      },
-    );
-
-    this.createTables();
-  }
+interface IStorageModel {
+  // Storage model
 }
+
+const legacyStorage = new LegacyStorage();
+
+const storage = AsyncStorageFactory.create<IStorageModel>(legacyStorage, {});
+
+export default storage;
